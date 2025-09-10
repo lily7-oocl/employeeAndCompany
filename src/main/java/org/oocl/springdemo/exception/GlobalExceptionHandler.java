697e3c371e76641357e24231ea69800b794601be
lyle.li <lily7@oocl.com>
2025-09-10 11:51:12 +0800

    feat:add GlobalExceptionHandler.java

---
+++ src/main/java/org/oocl/springdemo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.oocl.springdemo.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public String handleMyException(RuntimeException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public String handleMyException(ArithmeticException e) {
+        return e.getMessage();
+    }
+}
